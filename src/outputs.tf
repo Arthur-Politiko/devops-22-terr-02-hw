8983fdfa735d8ed0757a63e21654561b3ebb03bb
Kseniya <politiko-ks@narod.ru>
2025-09-11 21:39:44 +0300

    finish task 8

--- src/outputs.tf
+++ src/outputs.tf
@@ -5,3 +5,12 @@ output "vms" {
     fqdn: e.fqdn
   }]
 }
+
+output "task7" {
+  value = <<EOT
+  ${values(local.test_map)[0]} is ${keys(local.test_map)[0]} 
+  for ${keys(local.servers)[1]} server based on ${values(local.servers)[1].image} 
+  with ${values(local.servers)[1].cpu} vcpu, ${values(local.servers)[1].ram} GB ram 
+  and ${length(values(local.servers)[1].disks)} virtual disks
+  EOT
+}
--- src/variables.tf
+++ src/variables.tf
@@ -90,3 +90,28 @@ variable "vm_web_resources" {
   description = "https://yandex.cloud/ru/docs/data-proc/operations/cluster-create"
 
 }
+
+variable "test" {
+  type = list(map(list(string)))
+  default = [
+    {
+      "dev1" = [
+        "ssh -o 'StrictHostKeyChecking=no' ubuntu@62.84.124.117",
+        "10.0.1.7",
+      ]
+    },
+    {
+      "dev2" = [
+        "ssh -o 'StrictHostKeyChecking=no' ubuntu@84.252.140.88",
+        "10.0.2.29",
+      ]
+    },
+    {
+      "prod1" = [
+        "ssh -o 'StrictHostKeyChecking=no' ubuntu@51.250.2.101",
+        "10.0.1.30",
+      ]
+    }
+  ]  
+  description = "task 8"
+}
